ecc7d9eca4b9256dcc760d43778ea77a33c2fb0a
ajpetrizza <ajpetrizza@fullsail.edu>
2021-07-21 19:08:55 -0400

    setup our codebase

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.end();
+})
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
