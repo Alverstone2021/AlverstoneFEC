598fc8d0fc1545fa574b095034b64f4c21647abb
shaymarieee <s.marie0431@gmail.com>
2021-07-22 15:53:13 -0400

    created overviewIndex

---
+++ client/src/Overview/overviewIndex.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Overview = (props) => {
+  return (
+    <div>
+      <h1>Hey this is the Overview</h1>
+      <h3>wow so much to view</h3>
+      <button>amazing</button>
+    </div>
+  )
+}
+
+export default Overview;
