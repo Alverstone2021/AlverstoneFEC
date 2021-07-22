0a213c38671172942097174c1d1fa03d6a9ba817
bakese <sethlbaker09@gmail.com>
2021-07-22 16:33:36 -0400

    made that silly change for AJ

--- client/src/components/QA/QaIndex.jsx
+++ client/src/components/QA/QaIndex.jsx
@@ -4,7 +4,7 @@ const QaIndex = (props) => {
   return (
     <div>
       <h1>This is the QA section. deal with it</h1>
-      <h3>wow. so much qa here. much amazing</h3>
+      <h3>wow. so much qa here. much wow</h3>
     </div>
   )
 }
