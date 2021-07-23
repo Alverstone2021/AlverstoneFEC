30ab269f5a90cff0de5ab451d8b703d19710b134
ajpetrizza <ajpetrizza@fullsail.edu>
2021-07-23 16:13:22 -0400

    added basic network requests

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,44 +3,58 @@ import Overview from './components/Overview/overviewIndex.jsx'
 import RelatedProducts from './components/RelatedProducts/RelatedProducts.jsx'
 import QA from './components/QA/QaIndex.jsx'
 import Ratings from './components/Ratings/RatingsIndex.jsx'
-import axios from 'axios'
-import token from '../../config.js'
-import {useEffect, useState} from 'react'
+// import axios from 'axios'
+// import token from '../../config.js'
+import apiCalls from '../../helpers/shoppingApi.js';
+
+import { useEffect, useState } from 'react'
 
 const App = () => {
 
-  const [currentProduct, setCurrentProduct] = useState('')
-  const [allProducts, setAllProducts] = useState('')
+  const [currentProduct, setCurrentProduct] = useState('');
+  const [allQuestions, setAllQuestions] = useState([]);
+  const [allProducts, setAllProducts] = useState([]);
+  const [allRatings, setAllRatings] = useState([]);
 
+  // On mounting get all our data
   useEffect(() => {
 
-    var config = {
-      method: 'get',
-      url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/',
-      headers: {
-        'Authorization': token,
-      }
-    };
-
-    axios(config)
-    .then(function (response) {
-      // console.log(JSON.stringify(response.data[0]));
-      setCurrentProduct(response.data[0])
-      setAllProducts(response.data)
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-
-  }, [])
+    apiCalls.getAllProducts()
+      .then((products) => {
+        console.log('All products: ', products);
+        setCurrentProduct(products.data[0]);
+        setAllProducts(products.data);
+        // Get Q and A after initail products
+        apiCalls.getQandA(products.data[0].id)
+          .then((questions) => {
+            console.log('Questions: ', questions.data.results);
+            setAllQuestions(questions.data.results);
+          })
+          .catch((error) => {
+            console.log('Error fetching questions: ', error);
+          });
+        // get reviews
+        apiCalls.getRatings(products.data[0].id)
+          .then((ratings) => {
+            console.log('Reviews: ', ratings.data.results);
+            setAllRatings(ratings.data.results);
+          })
+          .catch((error) => {
+            console.log('Error fetching reviews: ', error);
+          })
+      })
+      .catch((error) => {
+        console.log('Error fetching all products');
+      });
+  }, []);
 
   return (
     <div className="hi">
       THE MASTER APP
-      <Overview currentProduct={currentProduct}/>
-      <RelatedProducts setAllProducts={setAllProducts} currentProduct={currentProduct}/>
-      <QA currentProduct={currentProduct}/>
-      <Ratings currentProduct={currentProduct}/>
+      <Overview currentProduct={currentProduct} />
+      <RelatedProducts setAllProducts={setAllProducts} currentProduct={currentProduct} />
+      <QA currentProduct={currentProduct} allQuestions={allQuestions} />
+      <Ratings currentProduct={currentProduct} allRatings={allRatings} />
     </div>
   );
 };
