ae00e05431c78a92cdeec68117183dc76d0ace80
ajpetrizza <ajpetrizza@fullsail.edu>
2021-07-24 19:47:07 -0400

    add average scores and recommended values

--- client/src/components/Ratings/RatingsIndex.jsx
+++ client/src/components/Ratings/RatingsIndex.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import ScoresList from './ScoresList.jsx';
 
 const RatingsIndex = (props) => {
   return (
     <div>
       <h1>This is my ratingsIndex app</h1>
-      <h3>More info here dog</h3>
+      <ScoresList allRatings={props.allRatings} />
     </div>
   );
 }
