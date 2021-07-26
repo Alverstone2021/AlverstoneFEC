dd58e00d4125a7d3d8aa796130f6c2ce1839cfd4
bakese <sethlbaker09@gmail.com>
2021-07-26 14:13:19 -0400

    styling updates

--- client/src/components/QA/QaAnswer.jsx
+++ client/src/components/QA/QaAnswer.jsx
@@ -5,10 +5,11 @@ import moment from 'moment';
 const QaAnswers = ({answer}) => {
   // console.log(answer)
   return (
-    <div>
+    <div class='answer'>
       <h3>A: {answer.body}</h3>
-      <h4>by {answer.answerer_name}, {moment(answer.date).format('MMMM Do YYYY')}  |  Helpful? Yes({answer.helpfulness})  |  Report</h4>
       <img src={answer.photos[0]} class="QaImage"></img>
+      <h4>by {answer.answerer_name}, {moment(answer.date).format('MMMM Do YYYY')}  |  Helpful? Yes({answer.helpfulness})  |  Report</h4>
+
     </div>
   )
 }
--- client/src/components/QA/QaQuestion.jsx
+++ client/src/components/QA/QaQuestion.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import QaAnswersList from './QaAnswersList.jsx'
+import QaAnswersList from './QaAnswersList.jsx';
+import moment from 'moment';
 
 const QaQuestion = ({question}) => {
   console.log(question)
   return (
-    <div>
+    <div class='question'>
 
       <div>
         <div>
           <h3><strong>Q: {question.question_body}</strong></h3>
-          <div>Helpful? Yes({question.question_helpfulness})</div>
-          <div>Add Answer</div>
+          <div>
+            <h4>{question.asker_name}, {moment(question.question_date).format('MMMM Do YYYY')} | Helpful? Yes({question.question_helpfulness}) | Add Answer | Report</h4>
+          </div>
         </div>
        <QaAnswersList answers={question.answers}/>
       </div>
