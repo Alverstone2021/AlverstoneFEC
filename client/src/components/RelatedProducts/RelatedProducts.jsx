ff54e4ef50aa90e6e2152de38c3ff72ac86554a6
Justin Case <justin.case1061@gmail.com>
2021-07-28 16:12:14 -0400

    related products tray no longer renders current product

--- client/src/components/RelatedProducts/RelatedProducts.jsx
+++ client/src/components/RelatedProducts/RelatedProducts.jsx
@@ -5,10 +5,21 @@ import OutfitTray from './OutfitTray.jsx';
 
 //represents the section that contains the related products tray and the outfit tray
 const RelatedProducts = (props) => {
+
+  const currentProductChecker = () => {
+    var products = [];
+    for (var i = 0; i < props.allProducts.length; i++) {
+      if (props.allProducts[i].id !== props.currentProduct.id) {
+        products.push(props.allProducts[i])
+      }
+    }
+    return products;
+  }
+
   return (
     <div>
       <h2>Start of Related Products</h2>
-      <RelatedProductsTray allProducts={props.allProducts} currentProduct={props.currentProduct}/>
+      <RelatedProductsTray allProducts={currentProductChecker()} currentProduct={props.currentProduct}/>
       <OutfitTray />
       <h2>End of Related Products</h2>
     </div>
