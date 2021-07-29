b0a0d652e10bbe6413e2ad1632b853330e64569b
Justin Case <justin.case1061@gmail.com>
2021-07-29 15:12:26 -0400

    added outfit list functionality

--- client/src/components/RelatedProducts/AddToOutfit.jsx
+++ client/src/components/RelatedProducts/AddToOutfit.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 
-const AddToOutfit = () => {
+const AddToOutfit = (props) => {
+
   return (
     <div>
-      PLUS
+
+      <div onClick={() => {
+        addToLocalStorage(props.currentProduct);
+        props.setTrigger(props.trigger + 1);
+      }}>
+        PLUS (clicking this adds something to the outfit.)
+      </div>
+      <div onClick={() => {
+        localStorage.clear();
+        props.setTrigger(props.trigger + 1)
+      }}>
+        CLICKING THIS CLEARS OUTFIT
+      </div>
     </div>
   )
 }
 
-export default AddToOutfit
+const addToLocalStorage = (item) => {
+  var stringifiedItemId = JSON.stringify(item.id)
+  localStorage.setItem(stringifiedItemId, JSON.stringify(item))
+}
+
+// onClick={() => {setTrigger(trigger+1)}}
+
+export default AddToOutfit;
--- client/src/components/RelatedProducts/OutfitCard.jsx
+++ client/src/components/RelatedProducts/OutfitCard.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
+import {useEffect, useState} from 'react';
+import apiCalls from '../../../../helpers/shoppingApi.js';
 
-const OutfitCard = () => {
+const OutfitCard = (props) => {
+
+  const [imageUrl, setImageUrl] = useState('');
+  const [cardStyles, setCardStyles] = useState({});
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    apiCalls.getStyles(props.product.id)
+      .then((styles) => {
+        var style = styles.data.results
+        for (var i = 0; i < style.length; i++) {
+            setCardStyles(style[i])
+            setImageUrl(style[i].photos[0].thumbnail_url || 'https://www.nomadfoods.com/wp-content/uploads/2018/08/placeholder-1-e1533569576673.png')
+        }
+      })
+  }, [])
+
   return (
-    <div>
-      outfit card
+    <div className='outfit-card'>
+      <img src={imageUrl} height='400' width='300'/>
+      <div>{props.product.category}</div>
+      <div>{props.product.name}</div>
+      <div>{props.product.default_price}</div>
     </div>
   )
 }
+
+{/* <div className='related-product-card' >
+      <button onClick={() => setShow(true) }>Compare</button>
+      <CompareModal currentProduct={props.currentProduct} productOnCard={productOnCard} onClose={() => setShow(false)} show={show}/>
+      <img src={imageUrl} height='400' width='300' onClick={() => props.setCurrentProduct(productOnCard)}/>
+      <div>{productOnCard.category}</div>
+      <div>{productOnCard.name}</div>
+      <div>{productOnCard.default_price}</div>
+      <div>5 Stars baby</div>
+    </div> */}
 
 export default OutfitCard
