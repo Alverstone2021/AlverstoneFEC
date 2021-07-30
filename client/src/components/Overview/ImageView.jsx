c85131fbc71854a167424f8540d5c2e248d4f934
shaymarieee <s.marie0431@gmail.com>
2021-07-30 15:45:37 -0400

    got extended view basics working!

--- client/src/components/Overview/ImageView.jsx
+++ client/src/components/Overview/ImageView.jsx
@@ -6,11 +6,26 @@ const ImageView = (props) => {
   return (
     <div className="image-view-container">
       <ImageCarousel className="carousel" productImageCarousel={props.productImageCarousel} currentStyle={props.currentStyle} setProductImage={props.setProductImage}/>
-      <div className="main-image-div">
+      <div className="main-image-div" onClick={(e) => {extendedView(e)}}>
         <img src={props.productImage} className="main-image"/>
       </div>
     </div>
   )
+  // IMAGE ENLARGER
+  function extendedView(e) {
+    e.preventDefault();
+    e.persist();
+    if (props.zoomClicked) {
+      props.setZoomClicked(false);
+      let div = document.getElementsByClassName("name-and-style-container");
+      div[0].style.display = "flex"
+    } else {
+      props.setZoomClicked(true);
+      let div = document.getElementsByClassName("name-and-style-container");
+      div[0].style.display = "none"
+    }
+
+  }
 }
 
 export default ImageView;
--- client/src/components/Overview/OverviewIndex.jsx
+++ client/src/components/Overview/OverviewIndex.jsx
@@ -12,6 +12,7 @@ const Overview = (props) => {
   const [productImageCarousel, setProductImageCarousel] = useState([]);
   const [productStyles, setProductStyles] = useState([]);
   const [currentStyle, setCurrentStyle] = useState({});
+  const [zoomClicked, setZoomClicked] = useState(false);
 
   useEffect(() => {
     apiCalls.getStyles(props.currentProduct.id || '19089')
@@ -33,11 +34,12 @@ const Overview = (props) => {
     <div className="overview-container">
       <AnnouncementBanner/>
       <div className="product-info-container">
-        <ImageView className="image-view-container" currentProduct={props.currentProduct} currentStyle={currentStyle} productStyles={productStyles} productImage={productImage} productImageCarousel={productImageCarousel} setProductImage={setProductImage}/>
+        <ImageView className="image-view-container" currentProduct={props.currentProduct} currentStyle={currentStyle} productStyles={productStyles} productImage={productImage} productImageCarousel={productImageCarousel} setProductImage={setProductImage} setZoomClicked={setZoomClicked} zoomClicked={zoomClicked}/>
         <div className="name-and-style-container">
-          <h4>{props.currentProduct.category}</h4>
-          <h1>{props.currentProduct.name}</h1>
-          <h5>${props.currentProduct.default_price}</h5>
+          <p className="rating">⭐️⭐️⭐️⭐️⭐️ </p>
+          <h4 className="category">{props.currentProduct.category}</h4>
+          <h1 className="product-name">{props.currentProduct.name}</h1>
+          <h5 className="price">${props.currentProduct.default_price}</h5>
           <StyleSelector className="style-selector" currentProduct={props.currentProduct} productStyles={productStyles} currentStyle={currentStyle} setCurrentStyle={setCurrentStyle} setProductImageCarousel={setProductImageCarousel} setProductImage={setProductImage}/>
         </div>
       </div>
