d2458954d8bcad24359377141669082d97894f2d
Justin Case <justin.case1061@gmail.com>
2021-07-31 18:42:25 -0400

    fixed bug that caused related product images not to render on first page load

--- client/src/components/RelatedProducts/RelatedProductsTray.jsx
+++ client/src/components/RelatedProducts/RelatedProductsTray.jsx
@@ -5,13 +5,18 @@ import RelatedProductCard from './RelatedProductCard.jsx'
 //Renders a tray that holds all the related product cards, still needs to have scrolling functionality
 const RelatedProductsTray = (props) => {
 
-  const [visibleProductIds, setVisibleProductIds] = useState(props.relatedProductIds)
+  const [visibleProductIds, setVisibleProductIds] = useState([])
   const [allProductIds, setAllProductIds] = useState([])
   const [leftIndex, setLeftIndex] = useState(0)
-  const [rightIndex, setRightIndex] = useState(3)
+  const [rightIndex, setRightIndex] = useState(4)
   const [trigger, setTrigger] = useState(0)
 
   useEffect(() => {
+    var renderedProductIds = [];
+    for (var i = leftIndex; i < rightIndex; i++) {
+      renderedProductIds.push(props.relatedProductIds[i])
+    }
+    setVisibleProductIds(renderedProductIds)
     setAllProductIds(props.relatedProductIds)
   }, [trigger])
 
@@ -21,21 +26,16 @@ const RelatedProductsTray = (props) => {
       renderedProductIds.push(props.relatedProductIds[i])
     }
     setVisibleProductIds(renderedProductIds)
-  }, [trigger])
-
-  useEffect(() => {
-    var renderedProductIds = [];
-    for (var i = leftIndex; i < rightIndex; i++) {
-      renderedProductIds.push(props.relatedProductIds[i])
-    }
-    setVisibleProductIds(renderedProductIds)
     setAllProductIds(props.relatedProductIds)
-  }, [])
+    // console.log(visibleProductIds)
+  })
 
+
+  /* click handlers */
   const leftClickHandler = () => {
-    if (leftIndex >= 0) {
-      console.log(props.relatedProductIds)
-      console.log(visibleProductIds)
+    if (leftIndex > 0) {
+      // console.log(props.relatedProductIds)
+      // console.log(visibleProductIds)
       setLeftIndex(leftIndex - 1)
       setRightIndex(rightIndex - 1)
       setTrigger(trigger + 1)
@@ -44,8 +44,6 @@ const RelatedProductsTray = (props) => {
 
   const rightClickHandler = () => {
     if (rightIndex <= props.relatedProductIds.length - 1) {
-      console.log(props.relatedProductIds)
-      console.log(visibleProductIds)
       setLeftIndex(leftIndex + 1)
       setRightIndex(rightIndex + 1)
       setTrigger(trigger + 1)
@@ -54,13 +52,13 @@ const RelatedProductsTray = (props) => {
 
   return (
     <div className='related-products-container'>
-      <div className='rp-right-arrow' onClick={() => {leftClickHandler()}}>Left Arrow</div>
+      <div className='rp-left-arrow' onClick={() => {leftClickHandler()}}>Left Arrow</div>
       <div className='related-product-tray'>
         {visibleProductIds.map((productId) => {
           return <RelatedProductCard trigger={trigger} setTrigger={setTrigger} productId={productId} key={productId} currentProduct={props.currentProduct} setCurrentProduct={props.setCurrentProduct} />
         })}
       </div>
-      <div className='rp-left-arrow' onClick={() => {rightClickHandler()}}>Right Arrow</div>
+      <div className='rp-right-arrow' onClick={() => {rightClickHandler()}}>Right Arrow</div>
     </div>
   )
 }
