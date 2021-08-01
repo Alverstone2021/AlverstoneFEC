d294c13ee994188b68366aa918e3a5e6d6146125
ajpetrizza <ajpetrizza@fullsail.edu>
2021-08-01 03:41:50 -0400

    added various sorting

--- client/src/components/Ratings/ReviewsList.jsx
+++ client/src/components/Ratings/ReviewsList.jsx
@@ -4,7 +4,16 @@ import ReviewListItem from './ReviewListItem.jsx';
 const ReviewsList = (props) => {
   return (
     <div className='review-grid-item3'>
-      <h3>{props.allRatings.length} reviews, sorted by relevance</h3>
+      <div className='review-dropdown'>
+        <h3>{props.allRatings.length} reviews, sorted by </h3>
+        <span className='review-dropspan'><h3>Relevance</h3>
+          <div className='review-dropdown-content'>
+            <p>Helpfulness</p>
+            <p>Newest</p>
+            <p>Relevance</p>
+          </div>
+        </span>
+      </div>
       <div className='reviewList-scrollable'>
         {props.allRatings.map((review, i) => {
           return <ReviewListItem key={i} review={review} />
