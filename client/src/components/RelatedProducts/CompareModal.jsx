65cc90a5e5f040c4212a54e29a4cf463bdf87b77
Justin Case <justin.case1061@gmail.com>
2021-08-03 14:28:52 -0400

    compareModal now actually compares and related product cards rendering default style image.  If no default, rpCards render first style image.

--- client/src/components/RelatedProducts/CompareModal.jsx
+++ client/src/components/RelatedProducts/CompareModal.jsx
@@ -7,10 +7,41 @@ const CompareModal = (props) => {
     return null
   }
 
-  useEffect(() => {
-    console.log('current prod', props.currentProduct)
-    console.log('on card', props.productOnCard)
-  }, [])
+  const tableMaker = () => {
+
+    var features = []
+    var current = props.currentProduct.features
+    var card = props.productOnCard.features
+
+    for (var i = 0; i < current.length; i++) {
+      if (current[i].feature !== undefined) {
+        var newObj = {}
+        newObj['feature'] = current[i].feature
+        newObj['currentProdValue'] = current[i].value
+        newObj['prodOnCard'] = '-'
+        features.push(newObj)
+      }
+    }
+
+    for (var j = 0; j < card.length; j++) {
+      for (var n = 0; n < features.length; n++) {
+        //if two products have the same feature
+        if (card[j].feature === features[n].feature) {
+          features[n]['prodOnCard'] = card[j].value
+        }
+        //if feature array does not contain feature
+        if (card[j].feature !== features[n].feature && n === features.length - 1) {
+          newObj = {}
+          newObj['feature'] = card[j].feature
+          newObj['currentProdValue'] = '-'
+          newObj['prodOnCard'] = card[j].value
+          features.push(newObj)
+        }
+      }
+    }
+
+    return features
+  }
 
   return (
     <div className="modal" onClick={props.onClose}>
@@ -19,19 +50,23 @@ const CompareModal = (props) => {
         <div className="modal-header">
           <h4 className="modal-title">Comparing</h4>
         </div>
-        <div className="modal-body">
-          <div className="current-product-left-column">
-            {props.currentProduct.name}
-          </div>
-          <div className="modal-checkmarks">
-            v
-          </div>
-          <div className="related-product-right-column">
-            {props.productOnCard.name}
-          </div>
-        </div>
+        <table className="modal-body-table">
+          <thead>
+            <tr>
+              <th>{props.currentProduct.name}</th>
+              <th>....X....</th>
+              <th>{props.productOnCard.name}</th>
+            </tr>
+          </thead>
+          {tableMaker().map((product) =>
+            <tr>
+              <td>{product.currentProdValue}</td>
+              <td>{product.feature}</td>
+              <td>{product.prodOnCard}</td>
+            </tr>
+          )}
+        </table>
         <div className="modal-footer">
-
         </div>
       </div>
     </div>
