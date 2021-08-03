6674a62c70df3b061d9192998df1a4ab448108b0
bakese <sethlbaker09@gmail.com>
2021-08-02 21:55:16 -0400

    Q's and A's sorted by helpfulness.

--- client/src/components/QA/QaQuestionsList.jsx
+++ client/src/components/QA/QaQuestionsList.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import QaQuestion from './QaQuestion.jsx'
 import { useEffect, useState } from 'react'
 
-const QaQuestionsList = ({allQuestions, qLimit, questions}) => {
+const QaQuestionsList = ({questions}) => {
+
+  questions = questions.sort((a, b) => {
+    return b.question_helpfulness - a.question_helpfulness
+  })
+
   return (
     <div className="questionsList">
       {questions.map(q => {
